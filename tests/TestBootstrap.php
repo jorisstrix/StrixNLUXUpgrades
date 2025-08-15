1263ce6c81526b2ac630715e21e104c06c77ebf1
jorisstrix <joris.van.os@strix.net>
2025-08-15 10:59:24 +0200

    initial commit

---
+++ tests/TestBootstrap.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+use Shopware\Core\TestBootstrapper;
+
+$loader = (new TestBootstrapper())
+    ->addCallingPlugin()
+    ->addActivePlugins('StrixAdvancedCMSExtensions')
+    ->setForceInstallPlugins(true)
+    ->bootstrap()
+    ->getClassLoader();
+
+$loader->addPsr4('StrixAdvancedCMSExtensions\\Tests\\', __DIR__);
